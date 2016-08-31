b4dc0b41b93236426c1b54d2d6cd16388b25c172
Lawrence Lam <lamlawrence10@gmail.com>
2016-08-31 00:26:22 -0700

    alternate sum prototype

---
+++ wilson_bootcamp/alternateSum.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+// Alterate adding and subtracting between an array of values.
+
+function alternateSum(n){
+    int sum = 0;
+    n.forEach(function(val, index){
+        //subtract odd elements, add even
+        if(index%2){
+            sum -= val;
+        } else{
+            sum += val;
+        }
+    });
+    return sum;
+};
+
+console.log(alternateSum([1,2,3,4,5,6,7]));
